6ab5019232d712392f0e9d437c95b2737213dca7
sutaotao <resuper0@163.com>
2020-07-07 20:28:54 +0800

    init project

---
+++ src/main/java/com/resuper/myeasyexcel/MyeasyexcelApplication.java
@@ -0,0 +1,13 @@
+package com.resuper.myeasyexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyeasyexcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyeasyexcelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/resuper/myeasyexcel/MyeasyexcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resuper.myeasyexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyeasyexcelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
